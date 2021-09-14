fe4861a9a1b5a586aa4612d51146130af725b125
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-09-14 16:40:05 -0300

    third commit

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,9 +27,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+				.csrf().disable()
 				.authorizeRequests()
-				.antMatchers("/", "index", "/css/*", "/js/*")
-				.permitAll()
+				.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+				.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name()) // Role based authentication
+				.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission()) // Permission based authentication 
+				.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+				.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+				.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
 				.anyRequest()
 				.authenticated()
 				.and()
@@ -42,10 +48,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		UserDetails annaSmithUser = User.builder()
 				.username("annasmith")
 				.password(passwordEncoder.encode("password"))
-				.roles("STUDENT") // ROLE_STUDENT
+				// .roles(ApplicationUserRole.STUDENT.name()) // ROLE_STUDENT
+				.authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
 				.build();
 		
-		return new InMemoryUserDetailsManager(annaSmithUser);		
+		UserDetails lindaUser = User.builder()
+				.username("linda")
+				.password(passwordEncoder.encode("password123"))
+				// .roles(ApplicationUserRole.ADMIN.name()) // ROLE_ADMIN
+				.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
+				.build();
+		
+		UserDetails tomUser = User.builder()
+				.username("tom")
+				.password(passwordEncoder.encode("password123"))
+				// .roles(ApplicationUserRole.ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+				.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
+				.build();
+		
+		return new InMemoryUserDetailsManager(
+				annaSmithUser,
+				lindaUser,
+				tomUser
+		);		
 	}
 	
 }
